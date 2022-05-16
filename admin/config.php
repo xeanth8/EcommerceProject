03c1b397a225195db2e6e953472ad8413d97368f
xeanth <56108002+xeanth8@users.noreply.github.com>
2022-05-16 14:24:25 +0700

    Installation

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/EcommerceProject/admin/');
+define('HTTP_CATALOG', 'http://localhost/EcommerceProject/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/EcommerceProject/admin/');
+define('HTTPS_CATALOG', 'http://localhost/EcommerceProject/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/Program/xampp/htdocs/EcommerceProject/admin/');
+define('DIR_SYSTEM', 'D:/Program/xampp/htdocs/EcommerceProject/system/');
+define('DIR_IMAGE', 'D:/Program/xampp/htdocs/EcommerceProject/image/');
+define('DIR_STORAGE', 'D:/Program/xampp/storage/');
+define('DIR_CATALOG', 'D:/Program/xampp/htdocs/EcommerceProject/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ecommerce_project');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
